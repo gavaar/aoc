a32d55e0bc8f60f2b264e03d1994a64354806ef7
Moi <oxspit@gmail.com>
2023-12-02 14:36:58 +0100

    day_02 part_02

--- src/advent/day_02.rs
+++ src/advent/day_02.rs
@@ -1,21 +1,28 @@
 use std::fs;
 
 struct Round {
-  pub blue: u8,
-  pub red: u8,
-  pub green: u8,
+  pub blue: usize,
+  pub red: usize,
+  pub green: usize,
 }
 
 struct Game {
   pub id: usize,
   pub rounds: Vec<Round>,
   pub is_possible: bool,
+  pub power: usize,
 }
 impl Game {
   fn new(line: &str, bag: &Round) -> Game {
     let mut split_line = line.split(':');
     let mut rounds = Vec::new();
     let mut is_possible = true;
+    let mut min_value = Round {
+      blue: 0,
+      green: 0,
+      red: 0,
+    };
+
     let id = split_line.next().unwrap().split(' ').nth(1).unwrap().parse::<usize>().expect("can't usize the game id");
     let plays_iter = split_line.next().unwrap().split(';');
 
@@ -46,15 +53,28 @@ impl Game {
         if round.blue > bag.blue || round.red > bag.red || round.green > bag.green {
           is_possible = false;
         }
+
+        if round.blue > min_value.blue {
+          min_value.blue = round.blue;
+        }
+        if round.green > min_value.green {
+          min_value.green = round.green;
+        }
+        if round.red > min_value.red {
+          min_value.red = round.red;
+        }
       }
 
       rounds.push(round);
     }
 
+    let power = min_value.red * min_value.blue * min_value.green;
+
     Game {
       id,
       rounds,
       is_possible,
+      power,
     }
   }
 }
@@ -81,11 +101,22 @@ fn add_game_ids(file: &str) -> usize {
   count
 }
 
+fn add_game_powers(file: &str) -> usize {
+  let games = parse_input(file, Round { blue: 0, green: 0, red: 0 });
+  let mut count = 0;
+  for game in games {
+    count += game.power;
+  }
+  count
+}
+
 pub fn run() {
   println!("### PART ONE ###");
   println!("-- Test --");
   println!("The value is: {}", add_game_ids("src/advent/day_02/test"));
+  println!("The power sum is: {}", add_game_powers("src/advent/day_02/test"));
   
   println!("-- Solution part 1 --");
   println!("The value of game 1 is: {}", add_game_ids("src/advent/day_02/input"));
+  println!("The power sum is: {}", add_game_powers("src/advent/day_02/input"));
 }
