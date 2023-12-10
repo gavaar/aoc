2a9e2ada9d2b0d8d38729db82ad26627da2f83ed
Moi <oxspit@gmail.com>
2023-12-10 23:25:54 +0100

    day_01 part_02

--- src/advent/day_10.rs
+++ src/advent/day_10.rs
@@ -1,4 +1,4 @@
-use crate::shared::{print_test, print_solution, read_input};
+use crate::shared::{print_test, print_solution, read_input, Color};
 
 // top, right, down, left
 fn pipe_connects(pipe: char) -> [bool; 4] {
@@ -36,6 +36,44 @@ impl Map {
 
     Map { map, starting_pos }
   }
+
+  pub fn starting_pipe(&self) -> char {
+    let top = self.top(self.starting_pos);
+    let right = self.right(self.starting_pos);
+    let bot = self.bot(self.starting_pos);
+    let left = self.left(self.starting_pos);
+
+    match (top.is_some(), right.is_some(), bot.is_some(), left.is_some()) {
+      (true, true, false, false) => 'L',
+      (true, false, true, false) => '|',
+      (true, false, false, true) => 'J',
+      (false, true, true, false) => 'F',
+      (false, true, false, true) => '-',
+      (false, false, true, true) => '7',
+      _ => unreachable!()
+    }
+  }
+
+  pub fn build_road(&self) -> Vec<(usize, usize)> {
+    let mut current_pos = self.starting_pos.clone();
+    let mut came_from = 9;
+    let mut steps: Vec<(usize, usize)> = Vec::new();
+
+    loop {
+      let (row, col, from) = self.find_next(current_pos, came_from);
+      came_from = from;
+      current_pos = (row, col);
+
+      if current_pos == self.starting_pos {
+        break;
+      }
+
+      steps.push((row, col));
+    }
+
+    steps
+  }
+
 
   fn pipe_in_pos(&self, origin: &(usize, usize)) -> char {
     self.map[origin.0][origin.1]
@@ -117,36 +155,102 @@ impl Map {
 
 fn part_one(chart: &String) {
   let map = Map::new(chart);
-  let mut current_pos = map.starting_pos.clone();
-  let mut came_from = 9;
-  let mut steps: Vec<(usize, usize)> = Vec::new();
+  let steps = map.build_road();
 
-  loop {
-    let (row, col, from) = map.find_next(current_pos, came_from);
-    came_from = from;
-    current_pos = (row, col);
+  println!("farthest: {}", Color::Red((steps.len() + 1) / 2));
+}
 
-    if current_pos == map.starting_pos {
-      break;
+fn part_two(chart: &String) {
+  let map = Map::new(chart);
+  let steps = map.build_road();
+  let mut burrow_size = 0;
+
+  for (row, line) in chart.lines().enumerate() {
+    let mut inside = false;
+    let mut expect = 'X';
+
+    for (col, dirty_char) in line.chars().enumerate() {
+      let char = if dirty_char == 'S' {
+        map.starting_pipe()
+      } else {
+        dirty_char
+      };
+
+      let is_in_map = map.starting_pos == (row, col) || steps.iter().find(|v| *v == &(row, col)).is_some();
+
+      if is_in_map {
+        if char == '|' {
+          inside = !inside;
+        }
+
+        if char == 'F' {
+          expect = 'J';
+        }
+
+        if char == 'L' {
+          expect = '7';
+        }
+
+        if char == 'J' && expect == 'J' {
+          inside = !inside;
+        }
+
+        if char == '7' && expect == '7' {
+          inside = !inside;
+        }
+
+        if char != '-' && char != 'F' && char != 'L' {
+          expect = 'X';
+        }
+      }
+
+      if is_in_map {
+        print!("{char}");
+      } else if inside {
+        burrow_size += 1;
+        print!("{}", Color::Green(char))
+      } else {
+        print!("{}", Color::Red(char))
+      }
     }
 
-    steps.push((row, col));
+    println!();
   }
 
-  println!("final: {:?}\nfarthest: {}", steps.last(), (steps.len() + 1) / 2);
+  println!("the size is {burrow_size}");
 }
 
 pub fn run() {
   print_test();
   let small_chart = read_input("day_10/test_1");
+  println!("small chart");
   part_one(&small_chart);
   println!();
-
+  
+  println!("medium chart");
   let medium_chart = read_input("day_10/test_2");
   part_one(&medium_chart);
   println!();
-  
+
+  println!("small enclose");
+  let small_enclose =read_input("day_10/test_3");
+  part_two(&small_enclose);
+  println!();
+
+  println!("medium enclose");
+  let mid_enclose =read_input("day_10/test_4");
+  part_two(&mid_enclose);
+  println!();
+
+  println!("large enclose");
+  let large_enclose =read_input("day_10/test_5");
+  part_two(&large_enclose);
+  println!();
+
   print_solution();
   let chart = read_input("day_10/input");
   part_one(&chart);
+
+  let enclose = read_input("day_10/input");
+  part_two(&enclose);
 }
