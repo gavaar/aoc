c21a8c72a29d95dc9e0bd7502231f6fbf1939c9b
Moi <oxspit@gmail.com>
2023-12-16 02:01:12 +0100

    no_day minor update to read_input

--- src/shared.rs
+++ src/shared.rs
@@ -6,7 +6,7 @@ pub use color::Color;
 pub use report_progress::report_progress;
 
 pub fn read_input(uri: &str) -> String {
-  let url = String::from("src/advent/") + uri;
+  let url = format!("src/advent/{}", uri);
   fs::read_to_string(url).expect("error reading file")
 }
 
