815348e33bd3371c1c737de051b879522e7b0f06
Moi <oxspit@gmail.com>
2024-12-01 20:08:29 +0100

    d01p02

--- src/advent/y2024/day_01.rs
+++ src/advent/y2024/day_01.rs
@@ -3,18 +3,31 @@ mod list_holder;
 use crate::shared::{print_solution, print_test, read_input, Color};
 use list_holder::ListHolder;
 
-fn part_one(uri: &str) {
+fn list_holder_from_uri(uri: &str) -> ListHolder {
   let input = read_input(uri);
-  let list_holder = ListHolder::new(&input);
-  
+  ListHolder::new(&input)
+}
+
+fn part_one(list_holder: &ListHolder) {
   let diff_vectors = list_holder.get_diffs_vector();
-  println!("Sum of differences is {}", Color::Green(diff_vectors.iter().sum::<u32>()));
+  println!("Sum of differences is {}", Color::Blue(diff_vectors.iter().sum::<u32>()));
+}
+
+fn part_two(list_holder: &ListHolder) {
+  let similarity_score = list_holder.similarity_score();
+  println!("The similarity score is {}", Color::Blue(similarity_score));
 }
 
 pub fn run() {
   print_test();
-  part_one("day_01/test");
-
+  let test_holder = list_holder_from_uri("day_01/test");
+  part_one(&test_holder);
+  part_two(&test_holder);
+  println!("");
+  
   print_solution();
-  part_one("day_01/input");
+  let list_holder = list_holder_from_uri("day_01/input");
+  part_one(&list_holder);
+  part_two(&list_holder);
+  println!("");
 }
--- src/advent/y2024/day_01/list_holder.rs
+++ src/advent/y2024/day_01/list_holder.rs
@@ -1,23 +1,36 @@
+use std::collections::HashMap;
+
 
 pub struct ListHolder {
   left: Vec<u32>,
   right: Vec<u32>,
+  similarity_map: HashMap<u32, u32>,
 }
 impl ListHolder {
   pub fn new(paper_ists: &String) -> ListHolder {
     let mut left: Vec<u32> = Vec::with_capacity(paper_ists.lines().count());
     let mut right: Vec<u32> = Vec::with_capacity(paper_ists.lines().count());
+    let mut similarity_map: HashMap<u32, u32> = HashMap::new();
 
     for line in paper_ists.lines() {
       let (left_value, right_value) = line.split_once("   ").expect("??");
-      left.push(left_value.parse::<u32>().expect(format!("left {left_value} value was not a number").as_str()));
-      right.push(right_value.parse::<u32>().expect(format!("left {left_value} value was not a number").as_str()));
+      let left_value_int = left_value.parse::<u32>().expect(format!("left {left_value} value was not a number").as_str());
+      let right_value_int = right_value.parse::<u32>().expect(format!("left {right_value} value was not a number").as_str());
+
+      left.push(left_value_int);
+      right.push(right_value_int);
+
+      if let Some(count) = similarity_map.get(&right_value_int) {
+        similarity_map.insert(right_value_int, count + 1);
+      } else {
+        similarity_map.insert(right_value_int, 1);
+      }
     }
 
     left.sort_unstable();
     right.sort_unstable();
 
-    return ListHolder { left, right }
+    return ListHolder { left, right, similarity_map }
   }
 
   pub fn get_diffs_vector(&self) -> Vec<u32> {
@@ -33,5 +46,13 @@ impl ListHolder {
     }
 
     diffs_vec
+  }
+
+  pub fn similarity_score(&self) -> u32 {
+    self.left.iter()
+      .map(|value| {
+        let similarity_value = self.similarity_map.get(value).unwrap_or(&0u32);
+        value * similarity_value
+      }).sum()
   }
 }
