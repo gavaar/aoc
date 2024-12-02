8a4493a2747f442a6c0c14265671bdfe3bf9f18d
Moi <oxspit@gmail.com>
2024-12-02 17:06:04 +0100

    d02p02 eww

--- src/advent/y2024/day_02.rs
+++ src/advent/y2024/day_02.rs
@@ -8,18 +8,36 @@ fn get_reports(uri: &str) -> Vec<Report> {
   files.lines().map(|line| Report::new(line)).collect()
 }
 
-fn part_one(reports: &Vec<Report>) {
-  let safe_reports = reports.iter().filter(|report| report.is_safe()).count();
+fn part_one(reports: &mut Vec<Report>) {
+  let safe_reports = reports.iter_mut().filter_map(|report| {
+    if report.is_safe() {
+      return Some(true);
+    }
+    None
+  }).count();
   println!("There are {} safe reports", Color::Blue(safe_reports));
+}
+
+fn part_two(reports: &mut Vec<Report>) {
+  let safe_reports = reports.iter_mut().filter_map(|report| {
+    report.activate_dampener();
+    if report.is_safe() {
+      return Some(true);
+    }
+    None
+  }).count();
+  println!("With dampener on: there are {} safe reports", Color::Blue(safe_reports));
 }
 
 pub fn run() {
   print_test();
-  let reports = get_reports("day_02/test");
-  part_one(&reports);
+  let mut reports = get_reports("day_02/test");
+  part_one(&mut reports);
+  part_two(&mut reports);
   println!();
   
   print_solution();
-  let reports = get_reports("day_02/input");
-  part_one(&reports);
+  let mut reports = get_reports("day_02/input");
+  part_one(&mut reports);
+  part_two(&mut reports);
 }
--- src/advent/y2024/day_02/report.rs
+++ src/advent/y2024/day_02/report.rs
@@ -1,43 +1,113 @@
 #[derive(Debug)]
 pub struct Report {
   files: Vec<u8>,
+  problem_dampener: bool,
 }
 impl Report {
   pub fn new(report_text: &str) -> Report {
     let files: Vec<u8> = report_text.split(" ").map(|value| value.parse::<u8>().expect("??")).collect();
-    Report { files }
+    Report { files, problem_dampener: false }
   }
 
-  pub fn is_safe(&self) -> bool {
-    // either no direction yet, positive or negative
+  pub fn activate_dampener(&mut self) {
+    self.problem_dampener = true;
+  }
+
+  pub fn is_safe(&mut self) -> bool {
     let mut direction: Option<bool> = None;
-    // prev number
-    let mut prev_number: Option<&u8> = None;
-    let mut files = self.files.iter();
+    let mut skipped = false;
 
-    while let Some(file) = files.next() {
-      // first one is skipped and set as prev_number
-      if prev_number == None {
-        prev_number = Some(file);
-        continue;
-      }
-      
-      if direction == None {
-        direction = if *file > *prev_number.unwrap() { Some(true) } else { Some(false) };
-      }
+    for i in 1..self.files.len() {
+      let curr = self.files.get(i).expect(format!("no file found: {i}").as_str());
+      let prev = self.files.get(i - 1).expect(format!("no file found: {}", i - 1).as_str());
 
-      let is_positive_direction = Some(true) == direction;
-      let prev_num_diff = *file as i16 - *prev_number.unwrap() as i16;
-      prev_number = Some(file);
+      if direction == None { direction = direction_calc(&curr, &prev) }
+      let is_safe = are_safe_values(&direction, *curr as i16, *prev as i16);
 
-      if is_positive_direction && (prev_num_diff < 1 || prev_num_diff > 3) {
-        return false;
-      }
-      if !is_positive_direction && (prev_num_diff > -1 || prev_num_diff < -3) {
+      if !is_safe {
+        if !self.problem_dampener || skipped {
+          return false
+        }
+
+        skipped = true;
+
+        let prev_prev = if i == 1 { None } else { self.files.get(i - 2) };
+        let next = self.files.get(i + 1);
+
+        let next_curr_safe = if next.is_none() {
+          // over last case
+          true
+        } else {
+          let mut temp_dir = direction.to_owned();
+          if i == 1 || i == 2 {
+            temp_dir = direction_calc(next.unwrap(), curr);
+          }
+          are_safe_values(&temp_dir, *next.unwrap() as i16, *curr as i16)
+        };
+        let curr_prev_prev_safe = if prev_prev.is_none() {
+          // below first case
+          true
+        } else {
+          let mut temp_dir = direction.to_owned();
+          if i == 1 || i == 2 {
+            temp_dir = direction_calc(curr, prev_prev.unwrap());
+          }
+          are_safe_values(&temp_dir, *curr as i16, *prev_prev.unwrap() as i16)
+        };
+        
+        let next_prev_safe = if next.is_none() {
+          // over last case
+          true
+        } else {
+          let mut temp_dir = direction.to_owned();
+          if i == 1 || i == 2 {
+            temp_dir = direction_calc(next.unwrap(), prev);
+          }
+          are_safe_values(&temp_dir, *next.unwrap() as i16, *prev as i16)
+        };
+        let prev_prev_prev_safe = if prev_prev.is_none() {
+          // before first case
+          true
+        } else {
+          let mut temp_dir = direction.to_owned();
+          if i == 1 || i == 2 {
+            temp_dir = direction_calc(prev, prev_prev.unwrap());
+          }
+          are_safe_values(&temp_dir, *prev as i16, *prev_prev.unwrap() as i16)
+        };
+
+        if next_curr_safe && curr_prev_prev_safe {
+          direction = None;
+          continue;
+        }
+
+        if next_prev_safe && prev_prev_prev_safe {
+          direction = None;
+          // swap so prev becomes curr and curr is destroyed in history
+          self.files.swap(i, i - 1);
+          continue;
+        }
+
         return false;
       }
     }
     
     return true;
+  }
+}
+
+fn direction_calc(file: &u8, prev_number: &u8) -> Option<bool> {
+  if file > prev_number { Some(true) } else { Some(false) }
+}
+
+fn are_safe_values(direction: &Option<bool>, file: i16, prev: i16) -> bool {
+  let is_positive_direction = &Some(true) == direction;
+
+  let prev_num_diff = file - prev;
+
+  if is_positive_direction {
+    prev_num_diff >= 1 && prev_num_diff <= 3
+  } else {
+    prev_num_diff <= -1 && prev_num_diff >= -3
   }
 }
