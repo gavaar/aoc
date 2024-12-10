4a567fd3036777cbb96e4944d08c9bf33e8e99c9
Francisco <oxspit@gmail.com>
2024-12-10 02:54:45 +0100

    d07p01

---
+++ src/advent/y2024/day_07.rs
@@ -0,0 +1,31 @@
+mod operation;
+
+use operation::Operation;
+use crate::shared::{print_solution, print_test, read_input, report_progress};
+
+fn build_operations(input: &String) -> Vec<Operation> {
+  input.lines().map(|line| Operation::new(line)).collect()
+}
+
+fn part_one(operations: &mut Vec<Operation>) {
+  let progress_total = operations.len();
+  operations.iter_mut().enumerate().for_each(|(progress_current, op)| {
+    report_progress(progress_current, progress_total);
+    op.find_solutions();
+  });
+  let valid_test: u128 = operations.iter().filter(|op| op.is_valid()).map(|op| op.expected_result).sum();
+  println!("The sum of valid tests is {valid_test}");
+}
+
+pub fn run() {
+  print_test();
+  let test_input = read_input("day_07/test");
+  let mut test_operations = build_operations(&test_input);
+  part_one(&mut test_operations);
+  println!();
+
+  print_solution();
+  let input = read_input("day_07/input");
+  let mut operations = build_operations(&input);
+  part_one(&mut operations);
+}
---
+++ src/advent/y2024/day_07/operation.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+
+pub struct Operation {
+  pub expected_result: u128,
+  components: Vec<u128>,
+  solutions: HashMap<String, u128>,
+}
+impl Operation {
+  pub fn new(line: &str) -> Operation {
+    let (expected, numbers) = line.split_once(": ").expect("should never fail");
+
+    Operation {
+      expected_result: expected.parse().expect("should be num"),
+      components: numbers.split(" ").map(|s| s.parse().unwrap()).collect(),
+      solutions: HashMap::new(),
+    }
+  }
+
+  pub fn find_solutions(&mut self) {
+    let mut components_iter = self.components.iter();
+    let first = components_iter.next().unwrap();
+    self.solutions.insert(String::new(), *first);
+
+    while let Some(next) = components_iter.next() {
+      let mut prev_step_solutions = self.solutions.keys();
+      let mut new_solutions = HashMap::new();
+
+      while let Some(solution) = prev_step_solutions.next() {
+        for oper in ['+', '*'] {
+          let value = self.solutions.get(solution).unwrap();
+          let next_oper = format!("{solution}{oper}");
+          let new_result = match oper {
+            '+' => value + next,
+            '*' => value * next,
+            _ => panic!("should not reach"),
+          };
+          new_solutions.insert(next_oper, new_result); 
+        }
+      }
+
+      self.solutions = new_solutions;
+    }
+  }
+
+  pub fn is_valid(&self) -> bool {
+    let result = self.solutions.iter().find(|&(_sol, value)| {
+      return value == &self.expected_result;
+    });
+
+    if result.is_some() {
+      return true;
+    }
+
+    return false
+  }
+}
