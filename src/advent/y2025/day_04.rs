6eac7c6d29c8828e46e9b89e1bd2b8fb9999d699
Moi <oxspit@gmail.com>
2025-12-05 00:20:08 +0100

    d04p02

--- src/advent/y2025/day_04.rs
+++ src/advent/y2025/day_04.rs
@@ -1,12 +1,16 @@
+use std::collections::HashSet;
+
 use crate::{components::{Map2D, Map2DDirection}, shared::{Color, read_input}};
 
 struct PrintingDpt {
   grid_map: Vec<Vec<char>>,
+  removed_map: HashSet<(usize, usize)>,
 }
 impl PrintingDpt {
   pub fn new(input: &String) -> PrintingDpt {
     PrintingDpt {
       grid_map: PrintingDpt::build_from_char_text(input),
+      removed_map: HashSet::new(),
     }
   }
 }
@@ -38,13 +42,50 @@ fn find_movable_rolls(map: &PrintingDpt) {
   println!("Rolls that can be moved: {}", Color::Green(can_be_moved));
 }
 
+fn remove_all_rolls(map: &mut PrintingDpt) {
+  loop {
+    let mut to_remove_values: HashSet<(usize, usize)> = HashSet::new();
+
+    for x in 0..map.cols() {
+      for y in 0..map.rows() {
+        let current = (x, y);
+        if map.get(&current).unwrap_or(&'.') == &'.' { continue; }
+
+        let roll_count = Map2DDirection::get_all_dirs()
+          .iter()
+          .map(|dir| map.get_at_dir(&current, &dir).unwrap_or((0,0,'.')))
+          .filter(|(_r, _c, value)| value == &'@')
+          .count();
+        if roll_count < 4 {
+          to_remove_values.insert(current);
+        }
+      }
+    }
+
+    if to_remove_values.is_empty() { 
+      break;
+    }
+
+    for removed in to_remove_values {
+      let row_to_modify = map.grid_map.get_mut(removed.1).expect("should exist");
+      row_to_modify[removed.0] = '.';
+
+      map.removed_map.insert(removed);
+    }
+  }
+}
+
 pub fn run() {
   let test_input = read_input("day_04/test");
-  let test_map = PrintingDpt::new(&test_input);
+  let mut test_map = PrintingDpt::new(&test_input);
   find_movable_rolls(&test_map);
+  remove_all_rolls(&mut test_map);
+  println!("removed: {}", test_map.removed_map.len());
 
   println!();
   let input = read_input("day_04/input");
-  let map = PrintingDpt::new(&input);
+  let mut map = PrintingDpt::new(&input);
   find_movable_rolls(&map);
+  remove_all_rolls(&mut map);
+  println!("removed: {}", map.removed_map.len());
 }
